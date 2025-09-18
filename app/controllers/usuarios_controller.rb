2d44e06033c4c6fc1f0f8f4aa9e5b5668976a382
João Barboza Mendonça <jbarbozamendonca@gmail.com>
2025-09-18 15:44:46 -0300

    update

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,19 +1,23 @@
 class UsuariosController < ApplicationController
   def index
     usuarios = Usuario.all
-
     render json: usuarios
   end
   
   def show
-    usuario = Usuario.find(params[:id])
-
-    render json: usuario
+    begin
+      usuario = Usuario.find(params[:id])
+      render json: usuario
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Usuário não encontrado' }, status: :not_found
+    end
   end
   
   def create
-    puts "Parâmetros recebidos: #{params.inspect}" # Adicione isso para logar os parâmetros recebidos
+    puts "Parâmetros recebidos: #{params.inspect}" # Para depuração
+
     usuario = Usuario.new(usuario_params)
+    
     if usuario.save
       render json: usuario, status: :created
     else
@@ -22,19 +26,31 @@ class UsuariosController < ApplicationController
   end
 
   def update
-    usuario = Usuario.find(params[:id])
-    if usuario.update(usuario_params)
-      render json: usuario
-    else
-      render json: usuario.errors, status: :unprocessable_entity
+    begin
+      usuario = Usuario.find(params[:id])
+      
+      if usuario.update(usuario_params)
+        render json: usuario
+      else
+        render json: usuario.errors, status: :unprocessable_entity
+      end
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Usuário não encontrado' }, status: :not_found
     end
   end
 
   def destroy
-    usuario = Usuario.find(params[:id])
-    usuario.destroy
+    begin
+      usuario = Usuario.find(params[:id])
+      usuario.destroy
+      render json: { message: 'Usuário deletado com sucesso' }, status: :ok
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Usuário não encontrado' }, status: :not_found
+    end
   end
   
+  private
+
   def usuario_params
     params.require(:usuario).permit(:nome, :email)
   end
